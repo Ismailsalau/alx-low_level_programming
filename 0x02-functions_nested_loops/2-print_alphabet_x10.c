fea614f2d5c08f77cec639ea088a2fe5b2e089a8
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-13 06:42:44 -0800

    file2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void);
 	char letter;
 	int i = 0;
 
-	while (i <= 9)
+	while (i++ <= 9)
 	{
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar('letter');
-		i++;
+		
 	_putchar('\n');
 	}
 }
