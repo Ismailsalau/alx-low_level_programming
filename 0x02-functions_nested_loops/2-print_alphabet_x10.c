f2bfde60b24c75fbafe819cfcfa214102e027f11
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-13 06:38:49 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lower case 10 times
+ */
+void print_alphabet_x10(void);
+{
+	char letter;
+	int i = 0;
+
+	while (i <= 9)
+	{
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar('letter');
+		i++;
+	_putchar('\n');
+	}
+}
