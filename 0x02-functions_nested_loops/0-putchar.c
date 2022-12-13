9fafbadc2c4922a88ebdac09a493f732124236b3
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-12 23:59:35 -0800

    12

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - program that prints _putchar
  *
+ *Written by Ismail
  * Return: 0
  *
  */
 int main(void)
 {
-	char;
-	putchar("\n_putchar");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
