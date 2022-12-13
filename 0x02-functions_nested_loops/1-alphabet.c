e26e83b931520542c3a7762c2cd3aee81820b2c8
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-13 05:51:10 -0800

    123

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet- print alphabet in lower case
+ *print_alphabet - print alphabet in lower case
  *
  */
 void print_alphabet(void);
@@ -11,6 +11,4 @@ void print_alphabet(void);
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 		_putchar('\n');
-
-	return (0);
 }
