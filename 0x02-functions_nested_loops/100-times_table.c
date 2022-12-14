cd08e6f80dec6e9d47043fae0c3f23ae2a0fbfb2
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-14 01:10:12 -0800

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,8 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 
 	{
-		_putchar('0');
+		for (num = 0; num <=n; num++)
+			_putchar('0');
 
 		for (mult = 1; mult <= n; mult++)
 		{
@@ -27,7 +28,7 @@ void print_times_table(int n)
 			if (prod >= 100)
 			{
 				_putchar((prod / 100) + '0');
-				_ptchar((prod / 100) % 10 + '0');
+				_putchar((prod / 100) % 10 + '0');
 			}
 			else if (prod <= 99 && prod >= 10)
 			{
