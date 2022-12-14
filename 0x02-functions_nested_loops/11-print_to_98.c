5cf425540a6f60efea31a3e7f32f8bf471800d71
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-13 23:49:20 -0800

    123

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,8 +6,9 @@
  * @n: The number to begin counting at.
  *
  */
-void print_to_98(int n)
+void print_to_98
 {
+	int n
 	if (n >= 98)
 	{
 		while (n > 98)
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,8 +9,9 @@
  * -1 and prints - if n is less than zero
  *
  */
-int print_sign(int n);
+int print_sign(int)
 {
+	int n;
 	if (n > 0)
 	{
 		_putchar('+');
