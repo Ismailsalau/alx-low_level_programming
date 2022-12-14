081a9407e1f290a3c5a3caf46808d2b87d0554de
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-14 02:33:45 -0800

    223

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,19 +6,19 @@
  * @n: The number to begin counting at.
  *
  */
-void print_to_98
+void print_to_98(int n)
 {
-	int n
 	if (n >= 98)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-			printf("%d\n", n);
+		printf("%d\n", n);
 	}
+
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-			printf("%d\n", n);
+		printf("%d\n", n);
 	}
 }
