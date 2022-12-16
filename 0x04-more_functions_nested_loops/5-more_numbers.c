a91f9fcf0cb2cf764382382bf627053ed83703fa
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-15 22:15:53 -0800

    12

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0-14 ten times
+ * @n: where to start printing from
+ *
+ */
+void more_numbers(void)
+{
+	int t = 0;
+
+	while (n >= 0 && n <= 14)
+
+	while (t++ <= 9)
+
+		if (n >= 14)
+			_putchar(n);
+
+
