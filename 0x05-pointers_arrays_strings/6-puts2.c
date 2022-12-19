3c7c903249365937f5226ac543a346864db3044c
Ismailsalau <ismailsalau2001@yahoo.com>
2022-12-19 11:07:24 -0800

    123

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: the string containing characters.
+ */
+void puts2(char *str)
+{
+	char *str;
+
+	str = "0123456789";
+	puts(str);
+	return (0);
+}
