5f9148cb13bfe5c1bb002bc6c4c1b12d86cb32d9
Ismailsalau <ismailsalau2001@yahoo.com>
2023-01-08 22:18:11 -0800

    12

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -> prints its name
+ * @argc: parameter
+ * @argv: an array of a listed command
+ *
+ * Return: 0
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
