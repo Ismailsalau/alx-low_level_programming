b3e4d86a6b91988357bc4f34b3565bb93d220c3c
Ismailsalau <ismailsalau2001@yahoo.com>
2023-01-12 20:45:03 -0800

    12

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -110,7 +110,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	for (; mult_len >= 0; mult_len--, mult--, prod--)
 	{
 		if (*mult < '0' || *mult > '9')
-		{	
+		{
 			printf("Error\n");
 			exit(98);
 		}
@@ -204,5 +204,7 @@ int main(int argc, char *argv[])
 	}
 	putchar('\n');
 	free(next_prod);
-	free(final_prod);								return (0);
+	free(final_prod);
+
+	return (0);
 }
