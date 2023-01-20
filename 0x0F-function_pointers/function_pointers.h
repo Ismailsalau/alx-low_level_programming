3526a5cd38ff6b409e9bab7e01ecb5cdeec5cedc
Ismailsalau <ismailsalau2001@yahoo.com>
2023-01-19 20:31:54 -0800

    22

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTION_POINTERS.H
-#define FUCTION_POINTERS.H
+#ifndef FUNCTION_POINTERS
+#define FUCTION_POINTERS
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* FUNCTION_POINTERS.H */
+#endif
