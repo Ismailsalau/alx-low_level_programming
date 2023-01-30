c3695c0d868f0fdd36a71c8c3539357c7c0c8244
Ismailsalau <ismailsalau2001@yahoo.com>
2023-01-30 11:05:07 -0800

    12

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,6 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *tmp, *node;
 	node = malloc(sizeof(listint_t));
+
 	if (node)
 	{
 		node->n = n;
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *tmp;
 	unsigned int i = 0;
+
 	tmp = head;
 
 	while (tmp != NULL && (i < index))
