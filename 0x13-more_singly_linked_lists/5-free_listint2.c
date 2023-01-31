fb6db7842bff2d814a70e622a98ebff53ae57f4e
Ismailsalau <ismailsalau2001@yahoo.com>
2023-01-30 20:56:32 -0800

    12

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *next;
 
-	while ((*head) != NULL)
+	if (head == NULL)
+		return;
+	while (*head != NULL)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-	free(*head);
-	*head = NULL;
 }
